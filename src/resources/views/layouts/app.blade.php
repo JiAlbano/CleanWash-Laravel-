4e9cce6398edb1a084e9e997028823e90ed4bda2
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-06 14:57:12 +0800

    Update app.blade.php (Bootstrap link api)

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -8,10 +8,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'CleanWash') }}</title>
-
+    
     <!-- Scripts -->
     <script src="{{ asset('assets/js/app.js') }}" defer></script>
-
+    <!-- Bootstrap API-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"/>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
