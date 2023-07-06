6f640c6b57a7d2474cf1bcb89756e15890441066
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-06 15:35:38 +0800

    Update viewAppointments.blade.php

--- src/resources/views/viewAppointments.blade.php
+++ src/resources/views/viewAppointments.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
+ <!-- Bootstrap API-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"/>
 <div class="container">
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="buff">
