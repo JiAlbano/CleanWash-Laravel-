a30290cf9a8f3615c658a0466004a78e9437a042
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-06 23:47:57 +0800

    Update home.blade.php update style again

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -34,10 +34,7 @@
 
 <style>
 body {
-     background: url(""../assets/homewp.png"") no-repeat center center fixed !important;
-    height: 100vh !important;
-    background-size: cover!important;
-    background-attachment: fixed!important;
-    background-position: center!important;
+    background: url("../assets/homewp.png") no-repeat center center fixed !important; /* Replace "../assets/login.jpeg" with the path to your desired image */
+    background-size: cover !important; /* Adjust the background image size to cover the entire body */
 }
 </style>
