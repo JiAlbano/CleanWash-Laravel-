6cee8c35c570d768dad15aafa7e47740db805f01
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-06 23:38:17 +0800

    Update welcome.blade.php Adding new style cover bg

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -131,3 +131,10 @@
         </div>
     </body>
 </html>
+
+<style>
+body {
+    background: url("../assets/login.png") no-repeat center center fixed !important; /* Replace "../assets/login.jpeg" with the path to your desired image */
+    background-size: cover !important; /* Adjust the background image size to cover the entire body */
+}
+</style>
