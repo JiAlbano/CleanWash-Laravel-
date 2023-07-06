638df7e361b14c661791e3ff3147dab7d4364559
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-06 23:39:16 +0800

    Update home.blade.php adding style bg cover

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -31,3 +31,10 @@
     </div>
 </div>
 @endsection
+
+<style>
+body {
+    background: url("../assets/login.png") no-repeat center center fixed !important; /* Replace "../assets/login.jpeg" with the path to your desired image */
+    background-size: cover !important; /* Adjust the background image size to cover the entire body */
+}
+</style>
