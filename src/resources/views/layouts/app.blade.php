eba506750f4857905f92a2d3032d1fa955c80bc6
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-13 15:07:04 +0800

    Update app.blade.php remove the local script

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <title>{{ config('app.name', 'CleanWash') }}</title>
     
     <!-- Scripts -->
-    <script src="{{ asset('assets/js/app.js') }}" defer></script>
     <script src="{{mix('js/app.js')}}"></script>
     <!-- Bootstrap API-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"/>
