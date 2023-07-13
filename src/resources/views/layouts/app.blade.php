7d27869608cc930b31ca78ac1b98dc4138327ff6
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-14 02:58:19 +0800

    Update app.blade.php

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
     <!-- Scripts -->
       <script src="{{mix('js/app.js')}}"></script>
     <script src="js/bootstrap.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<script src="jquery/jquery.js"></script>
+<script type="text/javascript" src='js/bootstrap.min.js'></script>
+<link rel="stylesheet" href="css/bootstrap.css" />
   
     <!-- Bootstrap API-->
     <link rel="stylesheet" href="css/bootstrap.min.css">
