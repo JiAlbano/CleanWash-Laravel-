b0496a2ff54f40662db8b39cba04ccb9bc89da61
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-14 03:13:03 +0800

    Update app.blade.php

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -10,11 +10,12 @@
     <title>{{ config('app.name', 'CleanWash') }}</title>
     
     <!-- Scripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"></link>
       <script src="{{mix('js/app.js')}}"></script>
     <script src="js/bootstrap.min.js"></script>
-<script src="jquery/jquery.js"></script>
-<script type="text/javascript" src='js/bootstrap.min.js'></script>
-<link rel="stylesheet" href="css/bootstrap.css" />
+
   
     <!-- Bootstrap API-->
     <link rel="stylesheet" href="css/bootstrap.min.css">
