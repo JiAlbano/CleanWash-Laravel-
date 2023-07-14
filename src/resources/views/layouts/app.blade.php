d4cb786ff5e1c6c7958440b1f052ef0ec2cea71b
JiAlbano <114860611+JiAlbano@users.noreply.github.com>
2023-07-14 12:22:51 +0800

    Update app.blade.php

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
     
     <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 
     <!-- Bootstrap API-->
